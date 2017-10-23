f315e907b5189d6615bf78c6814c77dbeb3dca1d
zuozhiw <zuozhiw@gmail.com>
2017-10-23 15:33:00 -0700

    include jointjs and its dependencies

--- core/new-gui/src/app/workspace/workflow-editor/workflow-editor.component.ts
+++ core/new-gui/src/app/workspace/workflow-editor/workflow-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as joint from 'jointjs';
+
 
 @Component({
   selector: 'texera-workflow-editor',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkflowEditorComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    const graph = new joint.dia.Graph;
+
+    const paper = new joint.dia.Paper({
+            el: $('#workspace-holder'),
+            width: 600,
+            height: 200,
+            model: graph,
+            gridSize: 1
+        });
+
+    const rect = new joint.shapes.basic.Rect({
+        position: { x: 100, y: 30 },
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+    graph.addCells([rect]);
+
+   }
 
   ngOnInit() {
   }

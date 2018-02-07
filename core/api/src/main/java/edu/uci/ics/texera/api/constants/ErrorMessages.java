e41886bda8c6ea929d7e3eb506b215b737b75eb1
Ming Huang <mingh2@uci.edu>
2018-02-06 21:47:57 -0800

    Add transformToOutputSchema to IOperator; Add appropriate error message

--- core/api/src/main/java/edu/uci/ics/texera/api/constants/ErrorMessages.java
+++ core/api/src/main/java/edu/uci/ics/texera/api/constants/ErrorMessages.java
@@ -12,6 +12,8 @@ public class ErrorMessages {
     public static final String SCHEMA_CANNOT_BE_NULL = "Schema cannot be null or empty";
     public static final String INPUT_OPERATOR_NOT_SPECIFIED = "Input operator is not specified";
     public static final String INPUT_OPERATOR_CHANGED_AFTER_OPEN = "Input Operator cannot be changed after opening the operator";
+    public static final String NUMBER_OF_ARGUMENTS_DOES_NOT_MATCH = "Number of arguments does not match. Expected: %d, Given: %d";
+    public static final String INVALID_INPUT_SCHEMA_FOR_SOURCE = "Source operator should not have input schema";
 
 
     public static final String DUPLICATE_ATTRIBUTE(Schema schema, String attributeName) {

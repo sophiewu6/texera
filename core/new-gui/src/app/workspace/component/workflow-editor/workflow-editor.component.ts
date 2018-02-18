8289db049f3e6735f48c533f6f7778074a1f919b
zuozhiw <zuozhiw@gmail.com>
2018-02-18 03:45:20 -0800

    play with workflow editor

--- core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
+++ core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import '../../../common/rxjs-operators.ts';
 
 declare var $: JQueryStatic;
 
@@ -7,7 +9,7 @@ import * as backbone from 'backbone';
 import * as joint from 'jointjs';
 
 import { OperatorDragDropService } from '../../service/operator-drag-drop/operator-drag-drop.service';
-import { WorkflowModelService } from '../../service/workflow-graph/workflow-model.service';
+import { WorkflowModelService } from '../../service/workflow-graph/model/workflow-model.service';
 import { WorkflowUIChangeService } from '../../service/workflow-graph/workflow-ui-change.service';
 
 
@@ -18,19 +20,44 @@ import { WorkflowUIChangeService } from '../../service/workflow-graph/workflow-u
 })
 export class WorkflowEditorComponent implements OnInit {
 
-  // the id in HTML must also be changed whenever this is changed
+  // the DOM element ID of the main editor
+  // the id in the corresponding html file must also be changed whenever this is changed
   readonly WORKFLOW_EDITOR_ID = 'texera-workflow-holder';
-  private paper: joint.dia.Paper;
+  private paper: joint.dia.Paper = this.createJointjsPaper();
+
+  // private elementSelectedInEditor$ = Observable.fromEventPattern(
+  //   this.paper.on(cell:pointerdown)
+  // )
+
+  public operatorSelectedInEditor = new Subject<string>();
+  public operatorDeletedInEditor = new Subject<string>();
+
+  public linkSelectedInEditor = new Subject<string>();
+  public linkDeletedInEditor = new Subject<string>();
+
 
   constructor(
     private workflowModelSerivce: WorkflowModelService,
     private workflowUIChangeService: WorkflowUIChangeService,
     private operatorDragDropService: OperatorDragDropService) {
-
   }
 
   ngOnInit() {
-    this.paper = new joint.dia.Paper({
+    // create the jointJS paper with custom configurations
+    // this.paper = this.createJointjsPaper();
+
+    // register the DOM element ID to drag and drop service
+    this.operatorDragDropService.registerDrop(this.WORKFLOW_EDITOR_ID);
+    // register the jointJS paper object to the workflow model
+    this.workflowModelSerivce.registerWorkflowPaper(this.paper);
+
+    // handle jointJS events
+    this.paper.on('cell:pointerdown', this.handleCellPointDown);
+    this.paper.on('element:delete', this.handleElementDelete);
+  }
+
+  private createJointjsPaper(): joint.dia.Paper {
+    const paper = new joint.dia.Paper({
       el: $('#' + this.WORKFLOW_EDITOR_ID),
       width: 600,
       height: 200,
@@ -38,38 +65,34 @@ export class WorkflowEditorComponent implements OnInit {
       gridSize: 1,
       snapLinks: true,
       linkPinning: false,
-      validateConnection: function (sourceView, sourceMagnet, targetView, targetMagnet) {
-
-        // user cannot draw connection starting from the input port (left side)
-        if (sourceMagnet && sourceMagnet.getAttribute('port-group') === 'in') {return false; }
-
-        // user cannot connect to the output port (right side)
-        if (targetMagnet && targetMagnet.getAttribute('port-group') === 'out') {return false; }
-
-        return sourceView.id !== targetView.id;
-        // return sourceMagnet !== targetMagnet;
-      }
+      validateConnection: this.validateOperatorConnection
     });
 
-    this.operatorDragDropService.registerDrop(this.WORKFLOW_EDITOR_ID);
+    return paper;
+  }
 
-    this.workflowModelSerivce.registerWorkflowPaper(this.paper);
+  private validateOperatorConnection(sourceView: joint.dia.CellView, sourceMagnet: SVGElement,
+    targetView: joint.dia.CellView, targetMagnet: SVGElement): boolean {
+    // user cannot draw connection starting from the input port (left side)
+    if (sourceMagnet && sourceMagnet.getAttribute('port-group') === 'in') { return false; }
 
-    this.paper.on('cell:pointerdown', (cellView, evt, x, y) => {
-      console.log('Cell pointer down occurrs');
-      this.workflowUIChangeService.selectOperator(cellView.model.id);
-    });
+    // user cannot connect to the output port (right side)
+    if (targetMagnet && targetMagnet.getAttribute('port-group') === 'out') { return false; }
 
+    return sourceView.id !== targetView.id;
+  }
 
-    // test delete action
-    this.paper.on('element:delete', (cellView, evt, x, y) => {
-      evt.stopPropagation();
-      // delete the operator in the workflow data
-      this.workflowUIChangeService.deleteOperator(cellView.model.id);
+  private handleCellPointDown(cellView: joint.dia.CellView, evt: Event, x: number, y: number) {
 
-      // delete the operator on the cellView
-      cellView.model.remove();
-    });
+  }
+
+  private handleElementDelete(cellView: joint.dia.CellView, evt: Event, x: number, y: number) {
+    // evt.stopPropagation();
+    // // delete the operator in the workflow data
+    // this.workflowUIChangeService.deleteOperator(cellView.model.id);
+
+    // // delete the operator on the cellView
+    // cellView.model.remove();
   }
 
 

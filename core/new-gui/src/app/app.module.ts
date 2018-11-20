53d4eda4ae04925020ebb386438708e1366bcc16
Zhaomin Li <zhaominl@uci.edu>
2018-11-19 21:32:31 -0800

    clear the commented code

--- core/new-gui/src/app/app.module.ts
+++ core/new-gui/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 

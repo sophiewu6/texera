2716037ae21328b305d7bbc00e42bdf7a2bc8c7d
sophiewu6 <wushiqi1998@gmail.com>
2020-02-20 14:26:24 -0800

    changed to sink

--- core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/visualization/lineplot/LinePlot.java
+++ core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/visualization/lineplot/LinePlot.java
@@ -6,91 +6,103 @@ import java.util.stream.Collectors;
 
 import edu.uci.ics.texera.api.constants.ErrorMessages;
 import edu.uci.ics.texera.api.constants.SchemaConstants;
+import edu.uci.ics.texera.api.dataflow.IOperator;
+import edu.uci.ics.texera.api.dataflow.ISink;
 import edu.uci.ics.texera.api.exception.DataflowException;
 import edu.uci.ics.texera.api.exception.TexeraException;
-import edu.uci.ics.texera.api.field.ListField;
-import edu.uci.ics.texera.api.schema.AttributeType;
 import edu.uci.ics.texera.api.schema.Schema;
-import edu.uci.ics.texera.api.span.Span;
 import edu.uci.ics.texera.api.tuple.Tuple;
-import edu.uci.ics.texera.dataflow.common.AbstractSingleInputOperator;
 import edu.uci.ics.texera.dataflow.visualization.lineplot.LinePlotPredicate;
 import edu.uci.ics.texera.dataflow.utils.DataflowUtils;
 
-public class LinePlot extends AbstractSingleInputOperator{
+public class LinePlot implements ISink {
 
     private final LinePlotPredicate predicate;
 
-    private String aggregateAttribute;
+    private IOperator inputOperator;
+
+    private Schema inputSchema;
+    private Schema outputSchema;
+
+    private int cursor = CLOSED;
+
+    private String xAxis;
 
     public LinePlot(LinePlotPredicate predicate) {
         this.predicate = predicate;
     }
 
-    @Override
-    protected void setUp() throws TexeraException {
-        if (inputOperator == null) {
-            throw new DataflowException(ErrorMessages.INPUT_OPERATOR_NOT_SPECIFIED);
+    public void setInputOperator(IOperator inputOperator) {
+        if (cursor != CLOSED) {
+            throw new TexeraException(ErrorMessages.INPUT_OPERATOR_CHANGED_AFTER_OPEN);
         }
-        this.outputSchema = inputOperator.getOutputSchema();
-//        aggregateAttribute = predicate.getXAxis();
+        this.inputOperator = inputOperator;
+    }
+
+    public IOperator getInputOperator() {
+        return this.inputOperator;
     }
 
     @Override
-    protected Tuple computeNextMatchingTuple() throws TexeraException {
-        Tuple inputTuple = null;
+    public Schema getOutputSchema() {
+        return outputSchema;
+    }
+
+    @Override
+    public void open() throws TexeraException {
+        if (cursor != CLOSED) {
+            return;
+        }
+        if (inputOperator == null) {
+            throw new TexeraException(ErrorMessages.INPUT_OPERATOR_NOT_SPECIFIED);
+        }
+        inputOperator.open();
+        inputSchema = inputOperator.getOutputSchema();
+        outputSchema = new Schema.Builder(inputSchema)
+                .removeIfExists(SchemaConstants.PAYLOAD).build();
+        cursor = OPENED;
+    }
+
+    @Override
+    public void processTuples() throws TexeraException {
+        return;
+    }
+
+    @Override
+    public Tuple getNextTuple() throws TexeraException {
+        if (cursor == CLOSED) {
+            return null;
+        }
+        if (cursor >= predicate.getLimit() + predicate.getOffset()) {
+            return null;
+        }
         Tuple resultTuple = null;
-
-        while ((inputTuple = inputOperator.getNextTuple()) != null) {
-            resultTuple = processOneInputTuple(inputTuple);
-
-            if (resultTuple != null) {
+        while (true) {
+            resultTuple = inputOperator.getNextTuple();
+            if (resultTuple == null) {
+                return null;
+            }
+            cursor++;
+            if (cursor > predicate.getOffset()) {
                 break;
             }
         }
-        return resultTuple;
+        return new Tuple.Builder(resultTuple)
+                .removeIfExists(SchemaConstants.PAYLOAD).build();
+
     }
 
     @Override
-    public Tuple processOneInputTuple(Tuple inputTuple) throws TexeraException {
-        // add payload if needed before passing it to the matching functions
-//        if (addPayload) {
-//            Tuple.Builder tupleBuilderPayload = new Tuple.Builder(inputTuple);
-//            tupleBuilderPayload.add(SchemaConstants.PAYLOAD_ATTRIBUTE, new ListField<Span>(
-//                    DataflowUtils.generatePayloadFromTuple(inputTuple, predicate.getLuceneAnalyzerString())));
-//            inputTuple = tupleBuilderPayload.build();
-//        }
-//
-//        // compute the keyword matching results
-//        List<Span> matchingResults = null;
-//        if (this.predicate.getMatchingType() == KeywordMatchingType.CONJUNCTION_INDEXBASED) {
-//            matchingResults =  appendConjunctionMatchingSpans(inputTuple, predicate.getAttributeNames(), queryTokenSet, predicate.getQuery());
-//        }
-//        if (this.predicate.getMatchingType() == KeywordMatchingType.PHRASE_INDEXBASED) {
-//            matchingResults = appendPhraseMatchingSpans(inputTuple, predicate.getAttributeNames(), queryTokenList, queryTokenWithStopwordsList, predicate.getQuery());
-//        }
-//        if (this.predicate.getMatchingType() == KeywordMatchingType.SUBSTRING_SCANBASED) {
-//            matchingResults = appendSubstringMatchingSpans(inputTuple, predicate.getAttributeNames(), predicate.getQuery());
-//        }
-//
-//        if (matchingResults.isEmpty()) {
-//            return null;
-//        }
-//
-        Tuple.Builder tupleBuilder = new Tuple.Builder(inputTuple);
-//        if (addResultAttribute) {
-//            tupleBuilder.add(predicate.getSpanListName(), AttributeType.LIST, new ListField<Span>(matchingResults));
-//        }
-        return tupleBuilder.build();
+    public void close() throws TexeraException {
+        if (cursor == CLOSED) {
+        }
+        if (inputOperator != null) {
+            inputOperator.close();
+        }
+        cursor = CLOSED;
     }
 
-    @Override
-    protected void cleanUp() {
-    }
-
-    public Schema transformToOutputSchema(Schema... inputSchema) {
-        if (inputSchema.length != 1)
-            throw new TexeraException(String.format(ErrorMessages.NUMBER_OF_ARGUMENTS_DOES_NOT_MATCH, 1, inputSchema.length));
-        return inputSchema[0];
+    public Schema transformToOutputSchema(Schema... inputSchema) throws DataflowException {
+        throw new TexeraException(ErrorMessages.INVALID_OUTPUT_SCHEMA_FOR_SINK);
     }
 }
